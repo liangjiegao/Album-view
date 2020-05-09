3539d6e34526b242c9876d825a235032fafa2e8b
Administrator <1445808283@qq.com>
2020-05-09 21:55:45 +0800

    NEW

--- layui/auth.js
+++ layui/auth.js
@@ -1,10 +1,27 @@
 let _tokenKey = 'token';
 // var host = 'http://localhost:';
 var host = 'http://120.27.210.67:';
- var prot = '8002';
-//var prot = '8001';
+ // var prot = '8002';
+var prot = '8001';
 
 function getToken() {
+  let token = getTokenInLocal();
+
+  if(token === '' || token == null){
+    alert('您未登录！');
+    var url = window.location.href;
+    var index = url.lastIndexOf('/');
+    var base = '';
+    if ( index > 0 ){
+      base = url.substring(0, index);
+    }
+    $(location).attr('href', base + '/login.html');
+  }
+  return token;
+}
+
+function getTokenInLocal() {
+
   let token = ''
   let tk = document.cookie;
   console.log("token" + tk);
@@ -16,18 +33,6 @@ function getToken() {
         token = list[1];
       }
     }
-  }
-
-
-  if(token === '' || token == null){
-    alert('您未登录！');
-    var url = window.location.href;
-    var index = url.lastIndexOf('/');
-    var base = '';
-    if ( index > 0 ){
-      base = url.substring(0, index);
-    }
-    $(location).attr('href', base + '/login.html');
   }
   return token;
 }
@@ -52,4 +57,27 @@ function checkLogin( code ) {
 
 
 
+}
+
+var ws = new WebSocket("wss://120.27.210.67/wss");
+ws.onopen = function(event){
+  console.log("open!");
+  let token = getTokenInLocal();
+  ws.send(token); //客户端给服务端推送消息
+
+};
+
+ws.onmessage= function(event){
+  console.log("message："+event.data);
+  // ws.send("15521166666"); //客户端给服务端推送消息
+};
+
+ws.onclose = function(event){
+  console.log("close");
+};
+var int=self.setInterval("clock()",5000);
+function clock()
+{
+  ws.send("ping"); //客户端给服务端推送消息
+  ws.onmessage
 }
